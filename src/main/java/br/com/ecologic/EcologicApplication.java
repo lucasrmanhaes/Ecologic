9c8145bbd2e0feb51319eacf4f35c69d26c0744c
lucas <ramosmanhaes@outlook.com>
2024-05-22 08:30:17 -0300

    Criando estrutura do projeto

---
+++ src/main/java/br/com/ecologic/EcologicApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecologic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcologicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcologicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ecologic/EcologicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ecologic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcologicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
